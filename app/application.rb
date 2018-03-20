8b3d4f733d5eacc3619aa60561ce98ff367eb7b4
Erin Walker <erinb0317@hotmail.com>
2018-03-20 23:40:52 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,13 +3,13 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    time = Time.hour.new
+    time = Time.new
     if time < 12
       resp.write "Good Morning"
     else
       resp.write "Good Afternoon"
     end
-    
+    resp.finish
   end
 
 end
